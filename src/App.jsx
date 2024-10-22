ff63f506bcbf867815d193b817fe2cca7a06da4c
jeewnish <jeewejan098@gmail.com>
2024-10-22 18:43:38 +0530

    nav bar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./layout.scss"
+import Navbar from "./component/navbar/Navbar.jsx";
+import React from "react";
+
+function App() {
+    return (
+        <div className="layout">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default App
