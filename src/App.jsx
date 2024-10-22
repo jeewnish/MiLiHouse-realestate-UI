d562a2140c2ac130fc941c7010f1631416847bea
jeewnish <jeewejan098@gmail.com>
2024-10-22 17:51:16 +0530

    f1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./layout.scss"
+import Navbar from "./component/navbar/Navbar.jsx";
+
+function App() {
+    return (
+        <div className="layout">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default App
