c9891713ec0bfb2bec178353f4a182d2c969004b
jeewnish <jeewejan098@gmail.com>
2024-10-25 12:31:58 +0530

    all files here

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import "./navbar.scss"
 
 function Navbar() {
+    const [open,setOpen] = useState(false)
     return(
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="left">
+
             <a href="/">
-                <img src="/logo.png" alt="logo" className="img-fluid" style={{height: "80px", width: "150px"}} />
+                <img src="/logo.png" alt="logo" className="logo"/>
             </a>
+
             <a href="/">Home</a>
             <a href="">About</a>
             <a href="">Contact</a>
             <a href="">Agents</a>
         </div>
         <div className="right">
-            <a href="">SignIn</a>
-            <a href="">SignUp</a>
+
+            <a href="" className="signin">SignIn</a>
+            <a href="" className="signup">SignUp</a>
+
+            <div className="menuIcon">
+                <img 
+                src="/menu.png" 
+                alt="" 
+                onClick={()=>setOpen((prev) => !prev)}/>
+            </div>
+
+            <div className={open ? "menu active" : "menu"}>
+                <a href="/">Home</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+                <a href="">Agents</a>
+                <a href="" className="signin">SignIn</a>
+            </div>
         </div>
     </nav>
     )
